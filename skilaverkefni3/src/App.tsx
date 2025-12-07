dd39685ba0901aa353a87056fd5f2f33bbe48800
Solon <asacornette@gmail.com>
2025-12-07 14:37:11 +0000

    lets go 2

--- skilaverkefni3/src/App.tsx
+++ skilaverkefni3/src/App.tsx
@@ -27,8 +27,10 @@ const questions = [
 function App() {
   const [currentNumber, setCurrentNumber] = useState(0);
 
-  const [answers, setAnswers] = useState<number[]>;
+  const [answers, setAnswers] = useState<(number | null)[]>([]);
   [];
+  const [showResults, setShowResults] = useState(false);
+
   const currentQuestion = questions[currentNumber];
 
   function handleSelectOption(selected: number) {
@@ -40,6 +42,8 @@ function App() {
   function handleNext() {
     if (currentNumber + 1 < questions.length) {
       setCurrentNumber(currentNumber + 1);
+    } else {
+      setShowResults(true);
     }
   }
 
@@ -49,21 +53,56 @@ function App() {
     }
   }
 
+  function handleRestart() {
+    setCurrentNumber(0);
+    setAnswers(questions.map(() => null));
+    setShowResults(false);
+  }
+
+  if (showResults) {
+    const correctCount = answers.filter(
+      (ans, i) => ans === questions[i].correct
+    ).length;
+
+    return (
+      <div className="App">
+        <h1>Þitt score</h1>
+        <p>
+          Þú fékkst <strong>{correctCount}</strong> af {""}
+          {questions.length} rétt!
+        </p>
+        <button onClick={handleRestart}>Reyna aftur</button>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <h1>React Quiz App</h1>
-      <p>Velkomin í quizið mitt!</p>
 
       <h2>{currentQuestion.question}</h2>
 
       <ul>
         {currentQuestion.options.map((option, number) => (
-          <li key={number}>{option}</li>
+          <li
+            key={number}
+            onClick={() => handleSelectOption(number)}
+            className={answers[currentNumber] === number ? "selected" : ""}
+          >
+            {option}
+          </li>
         ))}
       </ul>
 
-      <button onClick={handleNext}>Næsta Spurning</button>
-      <button onClick={handlePrevious}>Fara til baka</button>
+      <button onClick={handleNext}>
+        {" "}
+        {currentNumber + 1 === questions.length
+          ? "Sýna niðurstöður"
+          : "Næsta Spurning"}
+      </button>
+      <button onClick={handlePrevious} disabled={currentNumber === 0}>
+        Fara til baka
+      </button>
     </div>
   );
 }
